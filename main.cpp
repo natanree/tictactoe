ae85f5ef65bc11cb151a573f7a0844e5393109e9
natanree <natanreee@gmail.com>
2023-03-17 00:30:37 -0700

    added tests to main

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@ Date Last Modified: 03/16/2023
 */
 #include "GameBoard.hpp"
 #include "functions.hpp"
+#include "TestFunctions.hpp"
 
 int main (void)
 {
+    // test functions
+    TestFunctions testObj;
+    testObj.testResetBoard();
+    testObj.testUpdateBoard();
+    // end of test functions
+    
     GameBoard tictactoe;
     tictactoe.displayBoard();
     return 0;
