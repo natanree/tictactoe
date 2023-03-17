0a4ec8bc97ab2461c2aa6315f006d4db94ed6a2c
natanree <natanreee@gmail.com>
2023-03-16 23:30:41 -0700

    main menu

---
+++ functions.cpp
@@ -0,0 +1,8 @@
+#include "functions.hpp"
+
+void displayMenu(void)
+{
+    std::cout << "Please choose one of the following" << std::endl;
+    std::cout << "1. Play game" << std::endl;
+    std::cout << "2. Exit game" << std::endl;
+}
